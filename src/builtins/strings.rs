03c8f2fbb48b2edbbd340a9d53106a5149a3d1cd
Anand Krishnamoorthi <anakrish@microsoft.com>
2023-02-27 23:53:20 -0800

    strings: concat and contains
    Signed-off-by: Anand Krishnamoorthi <anakrish@microsoft.com>

--- src/builtins/strings.rs
+++ src/builtins/strings.rs
@@ -13,6 +13,7 @@ use anyhow::Result;
 
 pub fn register(m: &mut HashMap<&'static str, builtins::BuiltinFcn>) {
     m.insert("concat", concat);
+    m.insert("contains", contains);
 }
 
 fn concat(span: &Span, params: &[Expr], args: &[Value]) -> Result<Value> {
@@ -21,4 +22,12 @@ fn concat(span: &Span, params: &[Expr], args: &[Value]) -> Result<Value> {
     let delimiter = ensure_string(name, &params[0], &args[0])?;
     let collection = ensure_string_collection(name, &params[1], &args[1])?;
     Ok(Value::String(collection.join(&delimiter)))
+}
+
+fn contains(span: &Span, params: &[Expr], args: &[Value]) -> Result<Value> {
+    let name = "contains";
+    ensure_args_count(span, name, params, args, 2)?;
+    let s1 = ensure_string(name, &params[0], &args[0])?;
+    let s2 = ensure_string(name, &params[1], &args[1])?;
+    Ok(Value::Bool(s1.contains(&s2)))
 }
