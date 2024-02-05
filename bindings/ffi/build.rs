dda525b9890158bda07f36f51c586981bec0d61a
Anand Krishnamoorthi <35780660+anakrish@users.noreply.github.com>
2024-02-05 00:22:27 -0800

    C++ binding (#129)
    Signed-off-by: Anand Krishnamoorthi <anakrish@microsoft.com>

--- bindings/ffi/build.rs
+++ bindings/ffi/build.rs
@@ -17,10 +17,10 @@ fn main() {
     cbindgen::Builder::new()
         .with_crate(crate_dir)
         .with_language(cbindgen::Language::Cxx)
-        .with_include_guard("REGORUS_HPP")
+        .with_include_guard("REGORUS_FFI_HPP")
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file("regorus.hpp");
+        .write_to_file("regorus.ffi.hpp");
 
     csbindgen::Builder::default()
         .input_extern_file("src/lib.rs")
